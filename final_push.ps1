87bfb516dec93fd92a67aaff4f610341b20549bb
Pranayx24 <pranayyegireddy2004@gmail.com>
2026-04-05 23:21:57 +0530

    Cleanup: Removed unwanted log files, debug text, and temporary artifacts | Workspace optimization for GitHub Pages

--- final_push.ps1
+++
@@ -1,3 +0,0 @@
-git add js/views/scan-ultra.js 2>&1 | Out-File -FilePath "final_push_log.txt" -Encoding utf8
-git commit -m "FORCE PUSH: v22.1 overhaul" 2>&1 | Out-File -FilePath "final_push_log.txt" -Encoding utf8 -Append
-git push origin main 2>&1 | Out-File -FilePath "final_push_log.txt" -Encoding utf8 -Append
--- push_script.ps1
+++
@@ -1,6 +0,0 @@
-"FILE CONTENT CHECK" | Out-File -FilePath "run_log.txt" -Encoding utf8 -Append
-Get-Content js/views/scan-ultra.js | Select-String "torchState" | Out-File -FilePath "run_log.txt" -Encoding utf8 -Append
-git add js/views/scan-ultra.js 2>&1 | Out-File -FilePath "run_log.txt" -Encoding utf8 -Append
-git commit -v -m "Check changes" 2>&1 | Out-File -FilePath "run_log.txt" -Encoding utf8 -Append
-git push origin HEAD 2>&1 | Out-File -FilePath "run_log.txt" -Encoding utf8 -Append
-"FINISHED PUSH" | Out-File -FilePath "run_log.txt" -Encoding utf8 -Append
